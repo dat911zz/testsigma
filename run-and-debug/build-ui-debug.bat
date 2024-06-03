a6155d0e428ad649aea668f118f23d43d68de282
Vũ Ngô Đạt <datcy2011@gmail.com>
2024-06-03 10:17:23 +0700

    update

--- run-and-debug/build-ui-debug.bat
+++ run-and-debug/build-ui-debug.bat
@@ -2,5 +2,5 @@
 
 echo Starting to build Front-end...
 cd ../ui
-set NODE_OPTIONS=--openssl-legacy-provider
-npx ng build --source-map --configuration=docker
+set NODE_OPTIONS=--openssl-legacy-provider --max_old_space_size=4096
+npx ng build --source-map --watch --configuration=docker
--- ui/src/app/components/plugins/plugins.component.ts
+++ ui/src/app/components/plugins/plugins.component.ts
@@ -1,17 +1,57 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {NotificationsService, NotificationType} from 'angular2-notifications';
+import {TranslateService} from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationGuard } from 'app/shared/guards/authentication.guard';
+import { BaseComponent } from 'app/shared/components/base.component';
 
 @Component({
   selector: 'app-plugins',
   templateUrl: './plugins.component.html',
   styleUrls: ['./plugins.component.scss']
 })
-export class PluginsComponent implements OnInit {
+export class PluginsComponent extends BaseComponent implements OnInit {
 
   @Input('testInput') testInput: string;
-  constructor() { }
+  constructor(
+    public authGuard: AuthenticationGuard,
+    public notificationsService: NotificationsService,
+    public translate: TranslateService,
+    public toastrService: ToastrService) {
+    
+    super(authGuard, notificationsService, translate, toastrService);
+  }
 
   ngOnInit(): void {
-//     console.log("Kết quả nhận từ input: " + this.testInput);
+    super.showNotification(NotificationType.Error, "<b style='color: red'>Baka onii-chan?</b>");
+    super.showNotification(NotificationType.Alert, "Sussy baka?");
+    super.showNotification(NotificationType.Success, "Hehe :))");
+  }
+
+  showNotification(type: NotificationType, message, clickToClose?:boolean) {
+    if(type == NotificationType.Success){
+      super.showNotification(type, message);
+      return;
+    }
+    const temp = {
+      type: type,
+      title: message,
+      timeOut: 0,
+      showProgressBar: true,
+      pauseOnHover: true,
+      animate: 'fromLeft',
+      showCloseButton: true,
+      enableHtml: true,
+      clickToClose: false,
+      clickIconToClose: true,
+      theClass: "paused-notification",
+    };
+    let notification = this.notificationsService.create(temp.title, "", temp.type, temp);
+    notification.click.subscribe((event)=>{
+      if(event["target"] == document.querySelector(".icon")){
+        this.notificationsService.remove(notification.id);
+      }
+    })
   }
 
 }
